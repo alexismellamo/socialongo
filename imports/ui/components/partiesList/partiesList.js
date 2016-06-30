a1168c329178d120f3fb3c1f59305d01183b1655
Alexis Navarro <alexisnavarro23@gmail.com>
2016-06-30 00:18:31 -0500

    Router added, nav added

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import { name as PartyAdd } from '../partyAdd/partyAdd';
 import { name as PartyRemove } from '../partyRemove/partyRemove';
@@ -25,10 +26,22 @@ const name = 'partiesList';
 // create a module
 export default angular.module(name, [
   angularMeteor,
+  uiRouter,
   PartyAdd,
   PartyRemove,
 ]).component(name, {
   template,
   controllerAs: name,
   controller: PartiesList,
-});
+})
+.config(config);
+
+function config($stateProvider) {
+  'ngInject';
+  $stateProvider
+    .state('parties', {
+      url: '/parties',
+      template: '<parties-list></parties-list>',
+    });
+
+}
